fb0f14e821a88221ce14954a8351b5a1982704b9
kayobera13 <kayoberadieudonne92@gmail.com>
2023-05-26 11:57:57 +0300

    first commitfile with argument

--- Socket.c
+++ Socket.c
@@ -56,4 +56,86 @@ int main(int argc, char *argv[])
             abort ();
     }
 
+
 /* Open socket descriptor */
+if ( (server = socket( AF_INET, SOCK_STREAM, 0 )) < 0) {
+    perror("Can't create socket!");
+    return(1);
+}
+
+/* Fill local and remote address structure with zero */
+memset( &local_addr, 0, sizeof(local_addr) );
+memset( &remote_addr, 0, sizeof(remote_addr) );
+
+/* Set values to local_addr structure */
+local_addr.sin_family = AF_INET;
+local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+local_addr.sin_port = htons(cport);
+
+// set SO_REUSEADDR on a socket to true (1):
+optval = 1;
+setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+
+
+if ( bind( server, (struct sockaddr *)&local_addr, sizeof(local_addr) ) < 0 )
+{
+    /* could not start server */
+    perror("Bind problem!");
+    return(1);
+}
+
+if ( listen( server, SOMAXCONN ) < 0 ) {
+        perror("Listen");
+        exit(1);
+}
+
+printf("Concurrent socket server now starting!\n");
+printf("Wait for connection\n");
+
+while(1) {  // main accept() loop
+    length = sizeof remote_addr;
+    if ((fd = accept(server, (struct sockaddr *)&remote_addr, \
+          &length)) == -1) {
+          perror("Accept Problem!");
+          continue;
+    }
+
+    printf("Server: got connection from %s\n", \
+            inet_ntoa(remote_addr.sin_addr));
+
+
+    // Receive until the peer shuts down the connection
+    do {
+        // Clear Receive buffer
+        memset( &recvbuf, '\0', sizeof(recvbuf) );
+        rcnt = recv(fd, recvbuf, recvbuflen, 0);
+        if (rcnt > 0) {
+            printf("Bytes received: %d\n", rcnt);
+
+        // Echo the buffer back to the sender
+        rcnt = send( fd, recvbuf, rcnt, 0 );
+            if (rcnt < 0) {
+                printf("Send failed:\n");
+                close(fd);
+                break;
+            }
+            printf("Bytes sent: %d\n", rcnt);
+        } else if (rcnt == 0)
+            printf("Connection closing...\n");
+        else  {
+            printf("Receive failed:\n");
+            close(fd);
+            break;
+        }
+    } while (rcnt > 0);
+
+
+
+	//system_execution(fd);
+
+}
+
+// Final Cleanup
+close(server);
+
+}
